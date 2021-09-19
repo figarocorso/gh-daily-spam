d1f924db88fe83be97ed9045e90fbe03c1531cd1
Miguel Julián <figarocorso@users.noreply.github.com>
2021-09-19 17:41:48 +0200

    Open given PR in browser (#3)

--- ghdspam.py
+++ ghdspam.py
@@ -2,6 +2,8 @@ from github import Github
 from invoke import Context
 
 import json
+import os
+import webbrowser
 
 
 CONFIG_FILE = "config.json"
@@ -13,6 +15,10 @@ config = json.loads(invoke.run(f"AWS_PROFILE=figarocorso sops -d {CONFIG_FILE} |
                                hide=True, warn=True, echo=False).stdout)
 gh = Github(config["github_token"])
 repo = gh.get_repo(REPO)
-for pr in repo.get_pulls(state='open', sort='created', base='staging'):
-    if pr.user.login in config["team"]:
-        print(f"{pr.title} [{pr.user.login}]")
+while(True):
+    os.system("clear")
+    for pr in repo.get_pulls(state='open', sort='created', base='staging'):
+        if pr.user.login in config["team"]:
+            print(f"[{pr.number}] {pr.title} [{pr.user.login}]")
+    pr_number = int(input("\nPR number: "))
+    webbrowser.open_new_tab(repo.get_pull(pr_number).html_url)
