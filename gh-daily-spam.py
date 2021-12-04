1ab48cafc52ba374f3c53de708bd89fa4004c353
figarocorso <figarocorso@gmail.com>
2021-12-04 23:10:33 +0100

    Add multiple filters feature

--- gh-daily-spam.py
+++ gh-daily-spam.py
@@ -7,33 +7,39 @@ def main():
     prs = PullRequests()
     while(True):
         os.system("clear")
-        prs.retrieve_open_by_team()
+
         prs.print_prs()
-        pr_number = custom_input("\nPR number (0: exit | r: refresh) ")
 
-        if not pr_number:
-            pr_number = "1"
-
-        if pr_number == "r":
+        pr_number = get_pr_number_from_user()
+        if pr_number == "0":
             continue
 
-        if pr_number == "0":
-            exit()
+        operate_with_pr(prs, pr_number)
 
-        pr_number = int(pr_number)
-        prs.select(pr_number)
-        prs.open_selected()
-        prs.print_selected()
 
-        should_approve = custom_input("\nShould approve? (Y/n): ")
-        should_approve = not should_approve or should_approve != "n"
-        prs.approve_selected()
+def get_pr_number_from_user():
+    pr_number = custom_input("\nPR number (0: refresh): ")
+
+    if not pr_number:
+        pr_number = "1"
+
+    return int(pr_number)
+
+
+def operate_with_pr(prs, pr_number):
+    prs.select_pr(pr_number)
+    prs.open_selected()
+    prs.print_selected()
+
+    should_approve = custom_input("\nShould approve? (Y/n): ")
+    should_approve = not should_approve or should_approve != "n"
+    prs.approve_selected()
 
 
 def custom_input(message):
     try:
         return input(message)
-    except:
+    except:  # noqa
         print("\nBye!\n")
         exit()
 
--- src/pull_requests.py
+++ src/pull_requests.py
@@ -7,27 +7,75 @@ import webbrowser
 REPO = "draios/automation"
 ME = "figarocorso"
 APPROVED = "APPROVED"
+COMMENT = "COMMENT"
 
 
 class PullRequests:
+    filters = [
+        {"method": "retrieve_open_by_team", "description": "Opened PRs by team not yet reviewed"},
+        {"method": "retrieve_closed_by_team", "description": "Closed PRs by team not yet reviewed"},
+        {"method": "retrieve_open_at_onprem", "description": "Opened PRs at onprem folder not yet reviewed"},
+        {
+            "method": "retrieve_closed_at_onprem",
+            "description": "Closed PRs at onprem folder not yet reviewed"
+        },
+        {
+            "method": "retrieve_open_at_jenkins",
+            "description": "Opened PRs at jenkins folder not yet reviewed"
+        },
+    ]
+
     def __init__(self):
         self.prs = []
         self.selected_number = None
+        self.current_filter = 0
         self.config = Config()
         self.repo = Github(self.config.github_token).get_repo(REPO)
 
+    def print_prs(self):
+        while not self.prs and self.current_filter < len(self.filters):
+            print(f"Retrieving: {self.filters[self.current_filter]['description']}")
+            getattr(self, self.filters[self.current_filter]["method"])()
+            if not self.prs:
+                self.current_filter += 1
+
+            if not self.prs:
+                print("Job is over")
+                print("Bye!")
+                exit()
+
+            for index, pr in enumerate(self.prs):
+                print(f"[{index + 1}] {pr.title} [{pr.user.login}]")
+
     def retrieve_open_by_team(self):
         self.prs = [pr for pr in self.repo.get_pulls(state='open', sort='created', base='staging')
-                    # if True]
-                    if pr.user.login in self.config.team and not self.is_already_reviewed(pr)]
+                    if pr.user.login in self.config.team and self.pending_review(pr)]
 
-    def is_already_reviewed(self, pr):
+    def retrieve_closed_by_team(self):
+        self.prs = [pr for pr in self.repo.get_pulls(state="closed", sort="created", direction="desc",
+                                                     base="staging").get_page(0)
+                    if pr.user.login in self.config.team and self.pending_review(pr)]
+
+    def retrieve_open_at_onprem(self):
+        self.prs = [pr for pr in self.repo.get_pulls(state='open', sort='created', base='staging')
+                    if "onprem" in [label.name for label in pr.labels] and self.pending_review(pr)]
+
+    def retrieve_closed_at_onprem(self):
+        self.prs = [pr for pr in self.repo.get_pulls(state="closed", sort="created", direction="desc",
+                                                     base="staging").get_page(0)
+                    if "onprem" in [label.name for label in pr.labels] and self.pending_review(pr)]
+
+    def retrieve_open_at_jenkins(self):
+        self.prs = [pr for pr in self.repo.get_pulls(state='open', sort='created', base='staging')
+                    if "jenkins" in [label.name for label in pr.labels] and self.pending_review(pr)]
+
+    def pending_review(self, pr):
         for review in reversed([review for review in pr.get_reviews()]):
-            if review.user.login == ME and review.state == APPROVED:
-                return True
-        return False
+            if review.user.login == ME:
+                return False
+        return True
 
-    def select(self, position):
+    def select_pr(self, position):
         self.selected_number = position - 1
 
     @property
@@ -36,16 +84,6 @@ class PullRequests:
             raise "Ooops! There was no selected PR"
 
         return self.prs[self.selected_number]
-
-    def add(self, pr):
-        self.prs.append(pr)
-
-    def print_prs(self):
-        if not self.prs:
-            print("There are no PRs matching your filter")
-
-        for index, pr in enumerate(self.prs):
-            print(f"[{index + 1}] {pr.title} [{pr.user.login}]")
 
     def print_selected(self):
         pr = self.selected_pr
@@ -56,6 +94,6 @@ class PullRequests:
 
     def approve_selected(self):
         pull = self.repo.get_pull(self.selected_pr.number)
-        event = "COMMENT" if pull.state == "open" else "APPROVE"
-        body = ":+1:" if pull.state == "open" else "LGTM"
+        event = COMMENT if pull.state == "open" else APPROVED
+        body = "" if pull.state == "open" else "Just passing by, LGTM"
         pull.create_review(body=body, event=event)
