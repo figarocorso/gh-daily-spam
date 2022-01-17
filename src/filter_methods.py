d640047ca54e1f28d9bfabbc258489a8ff1feb11
figarocorso <figarocorso@gmail.com>
2022-01-17 23:42:09 +0100

    Avoid loading PRs list multiple times

--- src/filter_methods.py
+++ src/filter_methods.py
@@ -5,6 +5,21 @@ class FilterMethods:
     def __init__(self, repo, config):
         self.repo = repo
         self.config = config
+        self._open_prs = None
+        self._closed_prs = None
+
+    @property
+    def open_prs(self):
+        if self._open_prs is None:
+            self._open_prs = self.repo.get_pulls(state='open', sort='created', base='staging')
+        return self._open_prs
+
+    @property
+    def closed_prs(self):
+        if self._closed_prs is None:
+            self._closed_prs = self.repo.get_pulls(state="closed", sort="created", direction="desc",
+                                                   base="staging").get_page(0)
+        return self._closed_prs
 
     @property
     def default_filters(self):
@@ -17,31 +32,26 @@ class FilterMethods:
         ]
 
     def retrieve_open_by_team(self):
-        return [pr for pr in self.repo.get_pulls(state='open', sort='created', base='staging')
+        return [pr for pr in self.open_prs
                 if pr.user.login in self.config.team and self.pending_review(pr)]
 
     def retrieve_closed_by_team(self):
-        return [pr for pr in self.repo.get_pulls(state="closed", sort="created", direction="desc",
-                                                 base="staging").get_page(0)
+        return [pr for pr in self.closed_prs
                 if pr.user.login in self.config.team and self.pending_review(pr)]
 
     def retrieve_open_at_onprem(self):
-        return [pr for pr in self.repo.get_pulls(state='open', sort='created', base='staging')
-                if "onprem" in [label.name for label in pr.labels]
-                and self.pending_review(pr)
+        return [pr for pr in self.open_prs
+                if "onprem" in [label.name for label in pr.labels] and self.pending_review(pr)
                 and not self.author_is_me(pr)]
 
     def retrieve_closed_at_onprem(self):
-        return [pr for pr in self.repo.get_pulls(state="closed", sort="created", direction="desc",
-                                                 base="staging").get_page(0)
-                if "onprem" in [label.name for label in pr.labels]
-                and self.pending_review(pr)
+        return [pr for pr in self.closed_prs
+                if "onprem" in [label.name for label in pr.labels] and self.pending_review(pr)
                 and not self.author_is_me(pr)]
 
     def retrieve_open_at_jenkins(self):
-        return [pr for pr in self.repo.get_pulls(state='open', sort='created', base='staging')
-                if "jenkins" in [label.name for label in pr.labels]
-                and self.pending_review(pr)
+        return [pr for pr in self.open_prs
+                if "jenkins" in [label.name for label in pr.labels] and self.pending_review(pr)
                 and not self.author_is_me(pr)]
 
     def pending_review(self, pr):
