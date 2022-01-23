80e1ba71afc17c7ef7a6851d889e83948de7cecc
figarocorso <figarocorso@gmail.com>
2022-01-23 14:54:22 +0100

    Refactor user input method

--- src/pr_user_interaction.py
+++ src/pr_user_interaction.py
@@ -23,34 +23,35 @@ class PrUserInteraction:
         console.print(str(self.pr))
 
     def open_in_browser_workflow(self):
-        should_open = self.custom_input("\n[bold]Should open in browser? [reverse](Y/n)[/reverse]:[/bold] ")
+        should_open = self.custom_input("Should open in browser?", "(Y/n)")
         should_open = not should_open or should_open.lower() == "y"
         if should_open:
             self.pr.open_in_browser()
         return should_open
 
     def approve_workflow(self):
-        action = self.custom_input("\n[bold](A)pprove/(C)omment/(S)kip? [reverse](A/c/s)[/reverse]:[/bold] ")
+        action = self.custom_input("(A)pprove/(C)omment/(S)kip?", "(A/c/s)")
         action = action.lower() if action else "a"
 
         if action == "s":
             return
 
         if action == "c":
-            message = self.custom_input("\n[bold]Type custom message:[/bold] ")
+            message = self.custom_input("Type custom message:")
             self.pr.comment(message)
             return
 
         if action == "a":
-            should_custom_message = self.custom_input("\n[bold]Do you want custom message [reverse](y/N)[/reverse]:[/bold] ")
+            should_custom_message = self.custom_input("Do you want custom message?", "(y/N)")
             should_custom_message = should_custom_message and should_custom_message.lower() == "y"
-            message = self.custom_input("\n[bold]Type custom message:[/bold] ") if should_custom_message else ""
+            message = self.custom_input("Type custom message:") if should_custom_message else ""
             self.pr.approve(message)
 
     @classmethod
-    def custom_input(cls, message):
+    def custom_input(cls, message, options_message=""):
+        space = " " if options_message else ""
         try:
-            return console.input(message)
+            return console.input(f"\n[bold]{message} [reverse]{options_message}[/reverse][/bold]{space}")
         except:  # noqa
             console.print("\n\n[bold red]Bye![/bold red]\n")
             exit()
